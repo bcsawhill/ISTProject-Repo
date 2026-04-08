e5b5747e1f19763fd6363a4c3d7c7c511fe55fca
Dean <dmancini1999@gmail.com>
2026-04-08 12:39:16 -0400

    connected to atlas

--- config/mongodbconn.cjs
+++ config/mongodbconn.cjs
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb://localhost:27017/yogidb"
+const uri = "mongodb+srv://dmancini1999_db_user:eRifJAmRYwICGs5N@cluster0.uljjn4c.mongodb.net/yogidb?retryWrites=true&w=majority";
 
 mongoose.connect(uri)
 .then(() => console.log("✅ Connected to MongoDB"))
