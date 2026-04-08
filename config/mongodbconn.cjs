8e1d4f88dd4a64a4f96e0943270866342018cfd2
Dean <dmancini1999@gmail.com>
2026-04-08 13:03:33 -0400

    heroku work

--- config/mongodbconn.cjs
+++ config/mongodbconn.cjs
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://dmancini1999_db_user:eRifJAmRYwICGs5N@cluster0.uljjn4c.mongodb.net/yogidb?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI || "mongodb://localhost/yogidb";
+// const uri = "mongodb+srv://dmancini1999_db_user:eRifJAmRYwICGs5N@cluster0.uljjn4c.mongodb.net/yogidb?retryWrites=true&w=majority";
 
 mongoose.connect(uri)
 .then(() => console.log("✅ Connected to MongoDB"))
